88d51ad672c9e2f65c8d1a59cf593f3e7d88c3d9
Sergivaldo <sergivaldo123@gmail.com>
2021-10-03 14:56:39 -0300

    mvc e classe de teste da tarefa

---
+++ test/com/model/TarefaTest.java
@@ -0,0 +1,15 @@
+package com.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TarefaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		
+	}
+	
+}
