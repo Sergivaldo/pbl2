4b206fa7eb9b85f25bd44dd7731e82cef1db1ba7
Sergivaldo <sergivaldo123@gmail.com>
2021-10-16 16:13:00 -0300

    Implementação da página inicial e dos quadros da interface gráfica

---
+++ src/com/controller/paginaInicialController.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class paginaInicialController {
+	@FXML
+	private ScrollPane scroll;
+	@FXML
+	private VBox box;
+    
+    @FXML
+    private void addProjeto() {
+    	
+    }
+    
+}
+
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,32 +1,31 @@
 package com.main;
 	
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Iterator;
-
 import com.model.Quadro;
 import com.model.Tarefa;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	public void start(Stage primaryStage) throws IOException {
+		Pane root = FXMLLoader.load(getClass().getResource("/pagina_inicial.fxml"));
+		Scene scene = new Scene(root,640,480);
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 		
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	public static void main(String[] args) {
